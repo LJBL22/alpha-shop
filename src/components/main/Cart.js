a69c0b6f5093f7ca037e22b5a17a0e7d1e8d4997
LJBL22 <ljbl2208@gmail.com>
2023-02-16 09:15:03 +0800

    A4: map data

--- src/components/main/Cart.js
+++ src/components/main/Cart.js
@@ -1,38 +1,33 @@
 import React from "react";
 
-const Item = () => {
+const initialItems = [
+  {
+    id: "1",
+    name: "貓咪罐罐",
+    img: "https://picsum.photos/300/300?text=1",
+    price: 100,
+    quantity: 2,
+  },
+  {
+    id: "2",
+    name: "貓咪干干",
+    img: "https://picsum.photos/300/300?text=2",
+    price: 200,
+    quantity: 1,
+  },
+];
+
+const Item = ({ id, quantity, price, img, name }) => {
   return (
     <>
       <div
         class="product-container col col-12"
-        data-count="0"
-        data-price="3999"
+        data-count={quantity}
+        data-price={price}
       >
-        <img class="img-container" src="./images/product-1.jpg" alt="product" />
+        <img class="img-container" src={img} alt="product" />
         <div class="product-info">
-          <div class="product-name">破壞補丁修身牛仔褲</div>
-          <div class="product-control-container">
-            <div class="product-control">
-              <svg class="product-action minus">
-                <use xlinkHref="#svg-icon-minus"></use>
-              </svg>
-              <span class="product-count"></span>
-              <svg class="product-action plus">
-                <use xlinkHref="#svg-icon-plus"></use>
-              </svg>
-            </div>
-          </div>
-          <div class="price"></div>
-        </div>
-      </div>
-      <div
-        class="product-container col col-12"
-        data-count="0"
-        data-price="1299"
-      >
-        <img class="img-container" src="./images/product-2.jpg" alt="product" />
-        <div class="product-info">
-          <div class="product-name">刷色直筒牛仔褲</div>
+          <div class="product-name">{name}</div>
           <div class="product-control-container">
             <div class="product-control">
               <svg class="product-action minus">
@@ -66,13 +61,14 @@ const CartInfo = () => {
   );
 };
 
-// 這算是 cp. 中 有 cp 嗎？
 export default function Cart() {
   return (
     <section class="cart-container col col-lg-5 col-sm-12">
       <h3 class="cart-title">購物籃</h3>
       <section class="product-list col col-12" data-total-price="0">
-        <Item />
+        {initialItems.map((item) => (
+          <Item key={item.id} {...item} />
+        ))}
       </section>
       <CartInfo />
     </section>
